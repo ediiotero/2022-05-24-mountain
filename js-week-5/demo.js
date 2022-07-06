0cf999c1a73a6acfeabdc8a21d095c78fd4ebc13
ediiotero <e.otero81@gmail.com>
2022-07-05 20:43:36 -0400

    js week 6 ready

--- js-week-5/demo.js
+++ js-week-5/demo.js
@@ -1,3 +1,57 @@
 //https://www.youtube.com/watch?v=khuDeNwXkfI
 //https://www.youtube.com/watch?v=ZYa_NiOUTQo
 //https://www.youtube.com/watch?v=jM0WcyQWMSM
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    static sleep(num) {
+        console.log(`Slept ${num} hours`)
+    }
+
+    welcome() {
+        console.log(`Hello my name is ${this.name}`);
+    }
+}
+
+class Student extends Person {
+    constructor(name, age, gpa) {
+        super(name, age);
+        this.gpa = gpa;
+    }
+
+    hello() {
+        super.welcome();
+        console.log(`My gpa is ${this.gpa}`)
+    }
+}
+
+class Teacher extends Person {
+    constructor(name, age, classSize) {
+        super(name, age, classSize);
+        this.classSize = classSize;
+    }
+
+    hello() {
+        super.welcome();
+        console.log(`My class size is ${this.classSize}`)
+    }
+}
+
+const person1 = new Person("Bruce", 55);
+const person2 = new Person("Stephanie", 25);
+
+const student1 = new Student("Toby", 21, 4)
+const teacher1 = new Teacher("Ben", 65, 33)
+console.log(student1)
+
+teacher1.hello()
+Person.sleep(5)
+
+// console.log(person1)
+// console.log(person2)
+// person1.hello();
+// person2.hello();
